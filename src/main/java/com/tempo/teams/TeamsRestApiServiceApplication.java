af6ce8b29e0c17251ab7aa9101d5e70eaed93103
Christian <chris.sanchees@hotmail.com>
2021-12-03 21:55:55 -0300

    first commit

---
+++ src/main/java/com/tempo/teams/TeamsRestApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tempo.teams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsRestApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsRestApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tempo/teams/TeamsRestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempo.teams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsRestApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
