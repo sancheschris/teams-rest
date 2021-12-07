e46d7ea8c2ad5e1e962c0fabe985e8c9132113f9
Christian Santos <christian.d.santos@gmail.com>
2021-12-07 17:19:51 -0300

    1.1.12

--- src/test/java/com/tempo/teams/config/RestTemplateConfigTest.java
+++ src/test/java/com/tempo/teams/config/RestTemplateConfigTest.java
@@ -1,39 +1,41 @@
-//package com.tempo.teams.config;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.web.client.RestTemplateBuilder;
-//import org.springframework.web.client.RestTemplate;
-//import org.powermock.core.classloader.annotations.PrepareForTest;
-//import org.powermock.modules.junit4.PowerMockRunner;
-//
-//import java.time.Duration;
-//
-//import static org.junit.Assert.assertNotNull;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.*;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class RestTemplateConfigTest {
-//
-//    @InjectMocks
-//    private RestTemplateConfig restTemplateConfig;
-//
-//
-//    @Autowired
-//    private RestTemplateBuilder builder;
-//
-//
-////    @Test
-////    public void RestTemplateSuccess() {
-////
-////        RestTemplate restTemplate = restTemplateConfig.restTemplate(builder);
-////
-////        assertNotNull(restTemplate);
-////    }
-//}
+package com.tempo.teams.config;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RestTemplateConfigTest {
+
+    @InjectMocks
+    private RestTemplateConfig restTemplateConfig;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private RestTemplateBuilder builder;
+
+
+    @Test
+    public void RestTemplateSuccess() {
+        when(builder.build()).thenReturn(restTemplate);
+
+        RestTemplate restTemplate = restTemplateConfig.restTemplate(builder);
+
+        assertNotNull(restTemplate);
+    }
+}
